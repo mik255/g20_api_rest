19c3cfc0ceeab75335d8f13ff231736b8f010800
mik255 <mikael20informatica@gmail.com>
2022-04-27 12:34:06 -0300

    connetct

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ const mongoose = require("mongoose")
 var cors = require('cors');
 const mongoDbURI = process.env.MONGODB_URI
 const mongooseURIConnect = "mongodb+srv://Admin:Admin@cluster0.udlcp.mongodb.net/gvintedb?retryWrites=true&w=majority";
-mongoose.connect(mongooseURIConnect || mongooseURIConnect)
+mongoose.connect(mongoDbURI || mongooseURIConnect)
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
